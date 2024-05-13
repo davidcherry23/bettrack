c3f203460aec43e234e97cdabde7adc6406efe9d
davidcherry23 <davidcherry23@hotmail.co.uk>
2024-05-13 19:35:09 +0100

    Update script.js
    firebase

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 // Here you can add JavaScript to manipulate your bets
 console.log('Script loaded');
+const firebaseConfig = {
+  apiKey: "AIzaSyAaVOmHDYkG1qHxI6QpFpgM-B3MAQXY-2U",
+    authDomain: "bettrack-88281.firebaseapp.com",
+    projectId: "bettrack-88281",
+    storageBucket: "bettrack-88281.appspot.com",
+    messagingSenderId: "674702146250",
+    appId: "1:674702146250:web:5aec6047588594e163d233"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = firebase.firestore();
+
+function addSampleBet() {
+    db.collection("bets").add({
+        name: "Test Bet",
+        amount: 100
+    })
+    .then(docRef => {
+        alert("Document written with ID: " + docRef.id);
+    })
+    .catch(error => {
+        console.error("Error adding document: ", error);
+    });
+}
